1b381270f5ff23d59306d24ef9e29527a258f513
Kaung Set <kyawkaungset@gmail.com>
2021-06-10 16:46:24 +0630

    Initial commit

---
+++ logout.php
@@ -0,0 +1,13 @@
+ 
+
+
+ <?php 
+
+    if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login.php");
+  }
+
+
+  ?>
---
+++ success.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sucessfully Login</title>
+</head>
+<style>
+
+
+html, body 
+	{
+	    margin: 0;
+	    padding: 0;
+	    width: 100%;
+	    height: 100%;
+	    display: table;
+	}
+
+#content 
+	{
+	    display: table-cell;
+	    text-align: center;
+	    vertical-align: middle;
+	    border: 5px dotted red;
+	}
+
+p
+ {
+ 	font-size: 100px;
+ 	color: green;
+ }
+
+</style>
+<body>
+
+
+
+<div id="content">
+	<p><b>You are successfully login!</b></p>
+</div>
+
+</body>
+</html>
